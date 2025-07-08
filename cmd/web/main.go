0ac5fd24ee8d82101dae951400835b34626f4998
wynprtm <wayanpratama38@gmail.com>
2025-07-08 16:05:30 +0700

    refactor: :recycle: refactoring code and enable style and all the static file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,6 +14,22 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// buat sebuah variabel fileServer dengan http.FileServer yang langsung ke
+	// File static untuk UI menggunakan http.Dir() bisa langsung panggil dari root
+	// page.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Menambahkan endpoint /static/ untuk menyimpan file statis
+	// disini ketika sebuah permintaan berupa file di dalam static
+	// maka prefix /static akan dihilangkan
+	// contoh : ui/static/gambar.jpg
+	// maka karena StripPrefix("/static")
+	// hasilnya menjadi /gambar.jpg
+	// sehingga jika diteruskan ke fileServer
+	// maka akan mencari file dengan directory ./ui/static/gambar.jpg
+	// (mengabaikan // pada gambar.jpg)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Disini menggunakan log untuk mengeluarkan log mulai di server pada port 3000
 	// Untuk menjalankan webserver menggunakan http.ListenAndServe()
 	// Mengisikan nilai portnya kemudian variabel yang berisikan ServeMux() => mux
