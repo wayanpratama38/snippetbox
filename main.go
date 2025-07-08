fda60fd92f7d2e37d371ce71b23b8de6ffb1396e
wynprtm <wayanpratama38@gmail.com>
2025-07-08 13:51:07 +0700

    feat: :art: add new endpoint and handling missing routes

--- main.go
+++ main.go
@@ -8,7 +8,28 @@ import (
 // Ini handler home yang dibuat sebagai function.
 // Isinya  hanya sebuah string saja.
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// Karena bersifat catch-all maka setiap endpoint yang
+	// diakhiri dengan "/" akan langsung di route ke dalam function home
+	// bisa diatasi dengan conditional
+	if r.URL.Path != "/" {
+		// ini akan mengembalikan 404 ke user
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello From Snippetbox! Bro"))
+}
+
+// Ini sebuah function yang bertindak sebagai handler
+// untuk routing ke endpoint /snippet
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hellow from show snippet route!"))
+}
+
+// Ini sebuah function yang bertindak sebagai handler
+// untuk routing ke endpoint /snippet/create
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Creating new snippet..."))
 }
 
 func main() {
@@ -16,6 +37,9 @@ func main() {
 	// Terus memasukkan function home pada root "/".
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	// Menambahkan endpoint /snippet dan /snippet/create ke dalam webserver
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	// Disini menggunakan log untuk mengeluarkan log mulai di server pada port 3000
 	// Untuk menjalankan webserver menggunakan http.ListenAndServe()
